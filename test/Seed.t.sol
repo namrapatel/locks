7f7532bcc8535d9f3b294d682ebd206521687567
namrapatel <namrajpatel@gmail.com>
2022-09-27 11:20:45 -0400

    forge utils and test setup

--- test/Seed.t.sol
+++ test/Seed.t.sol
@@ -1,17 +1,34 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+// Foundry
 import "forge-std/Test.sol";
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import {Utilities} from "./utils/Utilities.sol";
+import {console} from "./utils/Console.sol";
+import {Vm} from "forge-std/Vm.sol";
+
+// Contracts
 import "../src/Seed.sol";
 import "../src/DummyContract.sol";
 
-contract SeedTest is Test {
+contract SeedTest is DSTestPlus {
+
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+    Utilities internal utils;
 
     Seed seed;
     DummyContract dummyContract;
 
     function setup() public {
+        utils = new Utilities();
+
         seed = new Seed();
         dummyContract = new DummyContract();
     }
+
+    function testOpcodeExists() public {
+        bool opcodeExists = seed.checkOpcodeExists(address(dummyContract));
+        vm.equal(opcodeExists, true, "Opcode exists");
+    }   
 }
