abe6095d0ac483d94397820799d9f560ccf877aa
namrapatel <namrajpatel@gmail.com>
2022-10-02 20:29:38 -0400

    funcsig + address test appears to pass on delegate call

--- test/LibDelegateCall.t.sol
+++ test/LibDelegateCall.t.sol
@@ -35,4 +35,23 @@ contract LibDelegateCallTest is DSTestPlus {
         blacklist[3] = mockDelegateCall.CALLEE();
         assertTrue(LibDelegateCall.checkCall(address(mockDelegateCall), blacklist));
     }
+
+      function testCheckDelegateCallAndFunction() public {
+        // Check that checkCallAndFunction returns true when submitted contract uses CALL opcode and calls an address in the blacklist
+        address[] memory blacklist = new address[](4);
+        blacklist[0] = address(0x0);
+        blacklist[1] = address(0x1);
+        blacklist[2] = mockDelegateCall.CALLER();
+        blacklist[3] = address(0xb0EdA4f836aF0F8Ca667700c42fcEFA0742ae2B5);
+        uint8[] memory functionsPerAddr = new uint8[](4);
+        functionsPerAddr[0] = 0;
+        functionsPerAddr[1] = 0;
+        functionsPerAddr[2] = 0;
+        functionsPerAddr[3] = 2;
+
+        bytes4[] memory functionSelectors = new bytes4[](1);
+        functionSelectors[0] = bytes4(keccak256("fakeFunc(uint256)"));
+
+        assertTrue(LibDelegateCall.checkCallAndFunction(address(mockDelegateCall), blacklist, functionsPerAddr, functionSelectors));
+    }
 }
